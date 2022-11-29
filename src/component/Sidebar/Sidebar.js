aa51744ef019a217e77fb46c8f058637b724ed32
User <fuadfahrezy216@gmail.com>
2022-11-29 11:18:20 +0700

    update sidebar

--- src/component/Sidebar/Sidebar.js
+++ src/component/Sidebar/Sidebar.js
@@ -21,42 +21,48 @@ const Sidebar = () => {
               </span>
             </Link>
             <li className="nav-item">
-              <Link to="/das" className="nav-link text-black ms-3 my-4">
+              <Link to="/dashboard" className="nav-link text-black ms-3 my-4">
                 Dashboard
               </Link>
             </li>
             <li className="nav-item">
-              <Link to="/kur" className="nav-link text-black ms-3 my-4">
+              <Link to="/kursus" className="nav-link text-black ms-3 my-4">
                 Kursus
               </Link>
             </li>
             <li className="nav-item">
-              <Link to="/mat" className="nav-link text-black ms-3 my-4">
+              <Link to="/materi" className="nav-link text-black ms-3 my-4">
                 Materi
               </Link>
             </li>
             <li className="nav-item">
-              <Link to="/men" className="nav-link text-black ms-3 my-4">
+              <Link to="/mentee" className="nav-link text-black ms-3 my-4">
                 Mentee
               </Link>
             </li>
             <li className="nav-item">
-              <Link to="/tu" className="nav-link text-black ms-3 my-4">
+              <Link to="/tugas" className="nav-link text-black ms-3 my-4">
                 Tugas
               </Link>
             </li>
             <li className="nav-item">
-              <Link to="/re" className="nav-link text-black ms-3 my-4">
+              <Link to="/review" className="nav-link text-black ms-3 my-4">
                 Review
               </Link>
             </li>
             <li className="nav-item">
-              <Link to="#" className="nav-link text-black ms-3 mt-4 mb-5">
+              <Link to="quiz" className="nav-link text-black ms-3 mt-4 mb-5">
                 Quiz
               </Link>
             </li>
             <li className="nav-item">
-            <button type="button" className="btn btn-outline-danger ms-3 px-5"><BsFillDoorOpenFill/><span className="ms-3 fw-bold">Keluar</span></button>
+              <button
+                type="button"
+                className="btn btn-outline-danger ms-3 px-5"
+              >
+                <BsFillDoorOpenFill />
+                <span className="ms-3 fw-bold">Keluar</span>
+              </button>
             </li>
           </ul>
         </div>
@@ -68,10 +74,10 @@ const Sidebar = () => {
         <div className="container-fluid">
           <div className="row">
             <div className="content">
-              <h1>Sidebar</h1>
-              <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-              </p>
+              <div className="heading"></div>
+              <h3 className="fw-bold text-center heading ms-4">
+                Sidebar
+              </h3>
             </div>
           </div>
         </div>
