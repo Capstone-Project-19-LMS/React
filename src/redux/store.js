9e305c9c021d2b87afdfd41e50dbbc1337bb636d
afauzann <afauzan857@gmail.com>
2022-12-16 12:56:09 +0700

    update: materi, courses, caregory slice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,26 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import listReducer from "./instructorSlice";
 import userReducer from "./instructorSlice";
 import courseReducer from "./courseSlice";
+import categoryReducer from "./coursesSlice";
+import coursesSlice from "./coursesSlice";
+import categorySlice from "./categorySlice";
+import materiSlice from "./materiSlice";
+
+// const store = configureStore({
+//   reducer: {
+//     user: userReducer,
+//     category: categoryReducer,
+//   },
+// });
+const reducer = combineReducers({
+  courses: coursesSlice,
+  category: categorySlice,
+  materi: materiSlice,
+});
 
 const store = configureStore({
-  reducer: {
-    user: userReducer,
-    course: courseReducer
-  },
+  reducer: reducer,
 });
 
 export default store;
