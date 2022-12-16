3bdd663f220bfccf9eabd9484fdf3ddeed293462
afauzann <afauzan857@gmail.com>
2022-12-16 12:52:42 +0700

    update: update token jwt login

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,3 +1,37 @@
+import Cookies from "js-cookie";
+
+// const Auth = {
+//   isAuthorization() {
+//     if (Cookies.get("token") || Cookies.get("rt")) return true;
+//     return null;
+//   },
+//   getAccessToken() {
+//     return Cookies.get("token");
+//   },
+//   getRefreshToken() {
+//     return Cookies.get("rt");
+//   },
+//   getUserId() {
+//     return Cookies.get("sub");
+//   },
+//   signOut(navigate) {
+//     Cookies.remove("token");
+//     Cookies.remove("rt");
+//     navigate("/login");
+//   },
+//   storeUserInfoToCookie(data) {
+//     if (!data.access || !data.refresh) return null;
+//     const { access, refresh, sub } = data;
+//     const accessExpires = new Date(access.expires);
+//     const refreshExpires = new Date(refresh.expires);
+//     Cookies.set("token", access.token, { expires: accessExpires });
+//     Cookies.set("rt", refresh.token, { expires: refreshExpires });
+//     Cookies.set("sub", sub, { expires: refreshExpires });
+//     return data;
+//   },
+// };
+
+// export default Auth;
 export const getIsAdmin = () => {
   return localStorage.getItem("isAdmin");
 };
@@ -11,6 +45,7 @@ export const getUserId = () => {
 
 export const getToken = () => {
   return localStorage.getItem("token") || null;
+  // return localStorage.getItem("token") || null;
 };
 
 export const removeUserSession = (navigate) => {
